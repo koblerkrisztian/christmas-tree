09fabf823f30e031e4feaa80c8adad18767fefd4
Krisztian Kobler <kobler.krisztian@gmail.com>
2018-12-13 00:30:57 +0100

    Rewamp Network and use end user setup

--- network.lua
+++ network.lua
@@ -1,15 +1,7 @@
 require("events")
+require("button")
 
--- load credentials, 'SSID' and 'PASSWORD' declared and initialize in there
-dofile("credentials.lua")
-
--- Define WiFi station event callbacks
-wifi_connect_event = function(T)
-    print("Connection to AP("..T.SSID..") established!")
-    print("Waiting for IP address...")
-    if disconnect_ct ~= nil then disconnect_ct = nil end
-end
-
+Network = {}
 
 function checkInternet()
   net.dns.resolve("google.com", function(sk, ip)
@@ -24,51 +16,72 @@ function checkInternet()
   end)
 end
 
-wifi_got_ip_event = function(T)
-  -- Note: Having an IP address does not mean there is internet access!
-  -- Internet connectivity can be determined with net.dns.resolve().
-  print("Wifi connection is ready! IP address is: "..T.IP)
-  checkInternet()
+local function hasSavedAp()
+  local ap = wifi.sta.getapinfo()
+  if ap == nil or ap[1] == nil or ap[1].ssid == nil or ap[1].ssid == "" then
+    return false
+  else
+    return true
+  end
 end
 
-wifi_disconnect_event = function(T)
-  if T.reason == wifi.eventmon.reason.ASSOC_LEAVE then
-    --the station has disassociated from a previously connected AP
-    return
+local function needEndUserSetup()
+  if not hasSavedAp() or Button.isPressed() then
+    return true
+  else
+    return false
   end
-  -- total_tries: how many times the station will attempt to connect to the AP. Should consider AP reboot duration.
-  local total_tries = 5
-  print("\nWiFi connection to AP("..T.SSID..") has failed!")
+end
 
-  --There are many possible disconnect reasons, the following iterates through
-  --the list and returns the string corresponding to the disconnect reason.
-  for key,val in pairs(wifi.eventmon.reason) do
-    if val == T.reason then
-      print("Disconnect reason: "..val.."("..key..")")
-      break
+local function startEndUserSetup()
+  wifi.sta.clearconfig()
+  enduser_setup.start(function()
+    print("Enduser setup finshed successfully")
+  end,
+  function(err, str)
+    print("Enduser setup error #" .. err .. ": " .. str)
+  end)
+end
+
+function Network.init()
+  -- Register WiFi Station event callbacks
+  wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
+    print("Connection to AP("..T.SSID..") established!")
+  end)
+  wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
+    print("Wifi connection is ready! IP address is: "..T.IP)
+    Events.WifiConnected:post()
+    checkInternet()
+  end)
+  wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(T)
+    if T.reason == wifi.eventmon.reason.ASSOC_LEAVE then
+      --the station has disassociated from a previously connected AP
+      return
     end
-  end
+    -- total_tries: how many times the station will attempt to connect to the AP. Should consider AP reboot duration.
+    local total_tries = 5
+    print("\nWiFi connection to AP("..T.SSID..") has failed!")
+  
+    --There are many possible disconnect reasons, the following iterates through
+    --the list and returns the string corresponding to the disconnect reason.
+    for key,val in pairs(wifi.eventmon.reason) do
+      if val == T.reason then
+        print("Disconnect reason: "..val.."("..key..")")
+        break
+      end
+    end
 
-  if disconnect_ct == nil then
-    disconnect_ct = 1
+    Events.WifiDisconnected:post()
+ end)
+
+  if needEndUserSetup() then
+    startEndUserSetup()
   else
-    disconnect_ct = disconnect_ct + 1
-  end
-  if disconnect_ct < total_tries then
-    print("Retrying connection...(attempt "..(disconnect_ct+1).." of "..total_tries..")")
-  else
-    wifi.sta.disconnect()
-    print("Aborting connection to AP!")
-    disconnect_ct = nil
+    print("Connecting to WiFi access point...")
+    wifi.sta.connect()
   end
 end
 
--- Register WiFi Station event callbacks
-wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, wifi_connect_event)
-wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, wifi_got_ip_event)
-wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, wifi_disconnect_event)
+Network.init()
 
-print("Connecting to WiFi access point...")
-wifi.setmode(wifi.STATION)
-wifi.sta.config({ssid=SSID, pwd=PASSWORD})
--- wifi.sta.connect() not necessary because config() uses auto-connect=true by default
+return Network
