eece9e7d330f64380123291c71b28b184161495c
Krisztian Kobler <kobler.krisztian@gmail.com>
2018-12-15 22:29:34 +0100

    Add Advent pattern

---
+++ patterns/advent.lua
@@ -0,0 +1,91 @@
+require("time")
+require("events")
+
+local Advent = {}
+
+local buffer = nil
+local timer = nil
+local segmentNumber = 0
+local hue = 20
+
+local function getAdventNumber(mon, day, wday)
+  if mon < 11 then return 0 end
+
+  local daysLeft = 0
+  if mon == 11 then
+    daysLeft = 30 - day + 25
+  else
+    daysLeft = 25 - day
+  end
+  if daysLeft < 0 then return 4 end
+
+  local dayOfWeekCompensation = (7 - (wday - 1)) % 7
+
+  return math.max(4 - ((daysLeft - dayOfWeekCompensation) / 7 + 1), 0)
+end
+
+local function getSegmentNumber()
+  if not Time.getTimeSynced() then return 0 end
+  local t = Time.getLocalTime()
+  return getAdventNumber(t.mon, t.day, t.wday)
+end
+
+local function clearBuffer()
+  buffer:fill(0, 0, 0, 0)
+  buffer:set(25, {0, Lights.INTENSITY, 0, 0})
+  buffer:set(26, {0, Lights.INTENSITY, 0, 0})
+end
+
+function Advent.start()
+  buffer = ws2812.newBuffer(Lights.NUM_LEDS, Lights.NUM_COLORS)
+  clearBuffer()
+
+  timer = tmr.create()
+  timer:alarm(200, tmr.ALARM_AUTO, function(t)
+    local intensity = Lights.INTENSITY
+    for i=1, segmentNumber * 6 do
+      local color = {color_utils.hsv2grbw(hue, 224, intensity + (node.random(-2, 2) * intensity / 10))}
+      buffer:set(i, color)
+    end
+    ws2812.write(Lights.transform(Lights.transformation_vertical_horizontal, buffer))
+  end)
+end
+
+function Advent.stop()
+  buffer = nil
+  timer:unregister()
+  timer = nil
+end
+
+function Advent.pause()
+  if timer then
+    timer:stop()
+  end
+end
+
+function Advent.resume()
+  if timer then
+    timer:start()
+  end
+end
+
+local function updateSegmentNumber()
+  local num = getSegmentNumber()
+  if num < segmentNumber then
+    clearBuffer()
+  end
+  segmentNumber = num
+end
+
+Events.TimeSynced:subscribe(function()
+  updateSegmentNumber()
+  Time.setAlarm(0, 0, function()
+    updateSegmentNumber()
+  end)
+end)
+
+function Advent.setHue(h)
+  hue = h
+end
+
+return Advent
