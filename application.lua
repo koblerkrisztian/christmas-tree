5c0939b269d8143898bee2f18afa0272ddb8935c
Krisztian Kobler <kobler.krisztian@gmail.com>
2018-12-16 23:15:00 +0100

    Add LFS support

--- application.lua
+++ application.lua
@@ -1,3 +1,12 @@
+local init_fn = node.flashindex("_init")
+if not init_fn then
+  if file.exists("lfs.img") then
+    print("Loading LFS image")
+    node.flashreload("lfs.img")
+  end
+end
+init_fn()
+
 require("events")
 require("time")
 require("button")
