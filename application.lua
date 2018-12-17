8061ad5a0a768ecb0535e38cf981b9ebad60bc7c
Krisztian Kobler <kobler.krisztian@gmail.com>
2018-12-17 23:05:45 +0100

    Update LFS if lfs.img found

--- application.lua
+++ application.lua
@@ -1,8 +1,19 @@
+local lfsFileNew = "lfs.img"
+local lfsFileCurrent = "lfs-current.img"
+
+if file.exists(lfsFileNew) then
+  if file.exists(lfsFileCurrent) then
+    file.remove(lfsFileCurrent)
+  end
+  file.rename(lfsFileNew, lfsFileCurrent)
+  node.flashreload(lfsFileCurrent)
+end
+
 local init_fn = node.flashindex("_init")
 if not init_fn then
-  if file.exists("lfs.img") then
+  if file.exists(lfsFileCurrent) then
     print("Loading LFS image")
-    node.flashreload("lfs.img")
+    node.flashreload(lfsFileCurrent)
   end
 end
 init_fn()
