c6aaa92157fece54893baef0953fd9146ff77f6f
Krisztian Kobler <kobler.krisztian@gmail.com>
2019-12-03 23:51:56 +0100

    Add ButtonLongPress event
    With defualt timeout of 3 seconds

--- lfs/button.lua
+++ lfs/button.lua
@@ -2,21 +2,37 @@ require("events")
 
 Button = {}
 
+Button.LONG_PRESS_TIME_MS = 3000
+
 local buttonPin = 5
 local lastLevel = 1
+local longPressTimer = nil
+
+local function buttonDown()
+  longPressTimer:alarm(Button.LONG_PRESS_TIME_MS, tmr.ALARM_SINGLE, function()
+    Events.ButtonLongPress:post()
+  end)
+  Events.ButtonDown:post()
+end
+
+local function buttonUp()
+  longPressTimer:unregister()
+  Events.ButtonUp:post()
+end
 
 function Button.init()
   gpio.mode(buttonPin, gpio.INT)
   gpio.trig(buttonPin, "both", function(level, timestamp)
     local level = gpio.read(buttonPin) -- Don't trust the provided level, it's bouncy
     if level == 0 and lastLevel == 1 then
-      Events.ButtonDown:post()
+      buttonDown()
       lastLevel = 0
     elseif level == 1 and lastLevel == 0 then
-      Events.ButtonUp:post()
+      buttonUp()
       lastLevel = 1
     end
   end)
+  longPressTimer = tmr.create()
 end
 
 function Button.isPressed()
--- lfs/events.lua
+++ lfs/events.lua
@@ -46,5 +46,6 @@ addEvent("TimeSynced")
 addEvent("LocationAcquired")
 addEvent("ButtonDown")
 addEvent("ButtonUp")
+addEvent("ButtonLongPress")
 
 return Events
